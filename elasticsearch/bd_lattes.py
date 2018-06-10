c1bfcb8023e523fb1efcb84d906a21fb1e47c963
Fernando Matsumoto <ferkmatsumoto@gmail.com>
2018-06-10 09:33:36 -0300

    3

--- elasticsearch/bd_lattes.py
+++
@@ -1,6 +0,0 @@
-{
-  "url_imagem": "http://servicosweb.cnpq.br/wspessoa/servletrecuperafoto?tipo=1&id=K4707323E3",
-  "bio": "Anna Sara Levin concluiu o doutorado em Doenças Infecciosas e Parasitárias pela Faculdade de Medicina da Universidade de São Paulo em 1996. Atualmente é Professora Associada da Faculdade de Medicina da Universidade de São Paulo. Publicou mais de 140 artigos em periódicos especializados. Possui 48 capítulos de livros e 11 livros publicados. Um dos livros "Guia de utilização de anti-infecciosos e recomendações para a prevenção de infecções relacionadas a assistência à saúde" está na sua 7a edição. Foi inicialmente concebido para ser um padrão para o Hospital das Clínicas porém passou a referência nacional. Orientou 15 dissertações de mestrado e 7 teses de doutorado na área de doenças Infecciosas. Recebeu 1 prêmio e/ou homenagem. Entre 2000 e 2015 participou de 6 projetos de pesquisa concluídos sendo coordenadora de 5 destes. Atualmente participa de projetos de pesquisa, que coordena, sendo financiada por agências de fomento. Atua na área de Medicina, em Doenças Infecciosas e Parasitárias com ênfase em infecção hospitalar e resistência microbiana. Seu e-mail para contato é gcih.adm@hc.fm.usp.br. (Fonte: Currículo Lattes)",
-  "email": "None",
-  "tel": "(11) 26617066"
-}
