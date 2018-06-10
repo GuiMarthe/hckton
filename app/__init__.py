b7030df65f1098badf51bfcd07764036e545f935
Gui <gui.marthe@gmail.com>
2018-06-10 02:57:16 -0300

    fixed flask bug

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.config.update(dict(
+    SECRET_KEY="powerful secretkey",
+    WTF_CSRF_SECRET_KEY="a csrf secret key"
+))
+
 from app import routes
