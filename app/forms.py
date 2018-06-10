136e770eea6f2157e28f48d5bfd0d9dc8b8fe7dd
Fernando Matsumoto <ferkmatsumoto@gmail.com>
2018-06-10 04:52:46 -0300

    templates

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
 
 class ProjectForm(FlaskForm):
     professor_name = StringField('Nome do Responsável', validators=[DataRequired()])
-    email = StringField('email', validators=[DataRequired()])
-    project_description = StringField('Descrição do Projeto', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    project_description = TextAreaField('Descrição do Projeto', validators=[DataRequired()])
     key_words = StringField('Palavras Chave', validators=[DataRequired()])
     project_title = StringField('Titulo do projeto', validators=[DataRequired()])
     submit = SubmitField('Adicionar projeto')
