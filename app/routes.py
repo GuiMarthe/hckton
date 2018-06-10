8cac5b691318434eccb6d83029c869694776722d
Gui <gui.marthe@gmail.com>
2018-06-10 03:06:10 -0300

    added get and post to add project

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,14 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import ProjectForm
 
-@app.route('/add_project/')
+@app.route('/add_project/', methods=['POST', 'GET'])
 def add_project():
     form = ProjectForm()
+        if form.validate_on_submit():
+        flash('Projeto {} adicionado.'.format(
+            form.project_title.data))
+        return redirect('/')
     return render_template('add_project.html', title='Adicione um projeto', form=form)
 
 # @app.route('/projects')
