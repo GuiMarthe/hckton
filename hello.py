f2bd926ce1508993c08e7575d3bac87975e01fa4
Gui <gui.marthe@gmail.com>
2018-06-10 01:27:10 -0300

    adicionar classe do jinja para formularios de projetos

--- hello.py
+++ hello.py
@@ -1,7 +1,14 @@
 from flask import render_template
+from flask import request
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/addProject/')
-def hello(name=None):
-    return render_template('add_project.html', name=name)
+@app.route('/add_project/', methods=['POST'])
+def add_project():
+    render_template('templates/add_project.html')
+    return request.form['nome']
+
+@app.route('/projects')
+def display_project():
+    pass
+
