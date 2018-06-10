23fdee9b772033bf4ff87a702d4e49855c59d9e9
Gui <gui.marthe@gmail.com>
2018-06-10 00:58:28 -0300

    again...

--- hello.py
+++ hello.py
@@ -2,7 +2,6 @@ from flask import render_template
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
+@app.route('/addProject/')
 def hello(name=None):
-    return render_template('hello.html', name=name)
+    return render_template('add_project.html', name=name)
